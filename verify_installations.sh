c892de906c9e16785200de4ea9e8b95746c35179
PJ <87932472+ngpinjie@users.noreply.github.com>
2024-07-11 13:54:18 +0800

    Update verify_installations.sh

--- verify_installations.sh
+++ verify_installations.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Create logs directory if it doesn't exist
-mkdir -p ./logs
-
 # Function to get CR status
 get_cr_status() {
   local component=$1
-  nohup cpd-cli manage get-cr-status \
+  cpd-cli manage get-cr-status \
     --cpd_instance_ns=${PROJECT_CPD_INST_OPERANDS} \
-    --components=${component} > "./logs/${component}_status.log" 2>&1 &
+    --components=${component}
 }
 
 # Read components from resources.txt and verify the status of each one
