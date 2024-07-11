8cd18c087459fdfecec18a302a4ff4acc9118167
PJ <87932472+ngpinjie@users.noreply.github.com>
2024-07-11 13:41:54 +0800

    Update install_services.sh
    Included nohup to allow installation to run in background
    Included logs to record the installation scripts

--- install_services.sh
+++ install_services.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Create logs directory if it doesn't exist
+mkdir -p ./logs
+
 # Function to login to OCP
 login_ocp() {
   cpd-cli manage login-to-ocp \
@@ -11,22 +14,22 @@ login_ocp() {
 # Function to apply OLM
 apply_olm() {
   local component=$1
-  cpd-cli manage apply-olm \
+  nohup cpd-cli manage apply-olm \
     --release=${VERSION} \
     --cpd_operator_ns=${PROJECT_CPD_INST_OPERATORS} \
-    --components=${component}
+    --components=${component} > "./logs/${component}_olm.log" 2>&1 &
 }
 
 # Function to apply CR
 apply_cr() {
   local component=$1
-  cpd-cli manage apply-cr \
+  nohup cpd-cli manage apply-cr \
     --components=${component} \
     --release=${VERSION} \
     --cpd_instance_ns=${PROJECT_CPD_INST_OPERANDS} \
     --block_storage_class=${STG_CLASS_BLOCK} \
     --file_storage_class=${STG_CLASS_FILE} \
-    --license_acceptance=true
+    --license_acceptance=true > "./logs/${component}_cr.log" 2>&1 &
 }
 
 # Read components from resources.txt and install each one
