4f7563cbbf0691775a3cff62cf8a3269ba5489cf
PJ <87932472+ngpinjie@users.noreply.github.com>
2024-07-11 13:43:59 +0800

    Update verify_installations.sh
    - Added nohup to allow installation to run in background
    - Added recording of logs in ./logs/ directory

--- verify_installations.sh
+++ verify_installations.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Create logs directory if it doesn't exist
+mkdir -p ./logs
+
 # Function to get CR status
 get_cr_status() {
   local component=$1
-  cpd-cli manage get-cr-status \
+  nohup cpd-cli manage get-cr-status \
     --cpd_instance_ns=${PROJECT_CPD_INST_OPERANDS} \
-    --components=${component}
+    --components=${component} > "./logs/${component}_status.log" 2>&1 &
 }
 
 # Read components from resources.txt and verify the status of each one
